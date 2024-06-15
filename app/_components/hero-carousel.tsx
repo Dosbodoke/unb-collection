2a2aa4c1c4c8b82c69f9e711e3a12022b0cc97b4
Juan Andrade <juangabriel4699@gmail.com>
2024-06-15 09:20:35 -0300

    Fix hero carousel image responsiveness

--- app/_components/hero-carousel.tsx
+++ app/_components/hero-carousel.tsx
@@ -27,6 +27,7 @@ const HeroCarousel = ({ items }: { items: string[] }) => {
         {items.map((item, index) => (
           <CarouselItem key={index + 1} className="bg-transparent p-0">
             <Image
+              className="mx-auto w-full max-w-md"
               width={375}
               height={437}
               src={item}
@@ -49,66 +50,3 @@ const HeroCarousel = ({ items }: { items: string[] }) => {
 };
 
 export { HeroCarousel };
-
-// "use client";
-
-// import {
-//   Carousel,
-//   CarouselIndicator,
-//   CarouselMainContainer,
-//   CarouselNext,
-//   CarouselPrevious,
-//   CarouselThumbsContainer,
-//   SliderMainItem,
-// } from "@/components/extension/carousel";
-// import Image from "next/image";
-
-// import { createClient } from "@/utils/supabase/server";
-
-// import Autoplay from "embla-carousel-autoplay";
-
-// const HeroCarousel = () => {
-//   // const supabase = createClient();
-
-//   // const promoList = await supabase.storage.from("promo").list();
-//   // const buckets = await supabase.storage.listBuckets();
-
-//   // console.log({ buckets });
-
-//   return (
-//     <Carousel
-//       carouselOptions={{ loop: true }}
-//       plugins={[
-//         Autoplay({
-//           delay: 100,
-//         }),
-//       ]}
-//     >
-//       <CarouselNext className="right-2" />
-//       <CarouselPrevious className="left-2" />
-//       <div className="relative">
-//         <CarouselMainContainer className="h-[437px]">
-//           {Array.from({ length: 3 }).map((_, index) => (
-//             <SliderMainItem key={index + 1} className="bg-transparent p-0">
-//               <Image
-//                 width={375}
-//                 height={437}
-//                 src={`/unb_collection_${index + 1}.png`}
-//                 alt="UNB Collection promo banner"
-//               />
-//             </SliderMainItem>
-//           ))}
-//         </CarouselMainContainer>
-//         <div className="absolute bottom-2 left-1/2 -translate-x-1/2">
-//           <CarouselThumbsContainer className="gap-x-1 ">
-//             {Array.from({ length: 3 }).map((_, index) => (
-//               <CarouselIndicator key={index} index={index} />
-//             ))}
-//           </CarouselThumbsContainer>
-//         </div>
-//       </div>
-//     </Carousel>
-//   );
-// };
-
-// export { HeroCarousel };
